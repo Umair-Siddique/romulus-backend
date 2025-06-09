2ed68a49117b43f276c936a1c0390c3003223d8f
Sharjeel Faiq <mrmalik610@gmail.com>
2025-06-09 10:43:07 +0500

    Removes OTP routes
    Removes the OTP routes.
    
    These routes are no longer needed in the application.

--- src/modules/OTP/otp.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import otpController from "./otp.controller.js";
-
-export const otpRoutes = express.Router();
-
-otpRoutes
-  .post("/send", otpController.send)
-  .post("/verify", otpController.verify);
