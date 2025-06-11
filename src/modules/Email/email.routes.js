68d7c5aaebfda01e3f428560f2322c95899cd94c
Sharjeel Faiq <mrmalik610@gmail.com>
2025-06-11 18:42:41 +0500

    Remove duplicate uppercase module directories

--- src/modules/Email/email.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { emailControllers } from "./email.controllers.js";
-
-export const emailRoutes = express.Router();
-
-emailRoutes
-  .get("/check-verification-token/:verificationToken", emailControllers.check)
-  .post("/send-verification-token", emailControllers.send);
