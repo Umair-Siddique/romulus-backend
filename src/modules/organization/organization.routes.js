91f2f7a952a6137cc32fae0c46b37d597790e3a9
Sharjeel Faiq <mrmalik610@gmail.com>
2025-06-13 22:09:25 +0500

    Replaces uploadFiles with uploadMiddleware in organization and user routes

--- src/modules/organization/organization.routes.js
+++ src/modules/organization/organization.routes.js
@@ -2,13 +2,13 @@ import express from "express";
 import { createOrganizationDto } from "#dtos/index.js";
 import { validateDto } from "#middleware/index.js";
 import { organizationControllers } from "./organization.controllers.js";
-import { uploadFiles } from "#middleware/index.js";
+import { uploadMiddleware } from "#middleware/index.js";
 
 export const organizationRoutes = express.Router();
 
 organizationRoutes.post(
   "/",
-  uploadFiles,
+  uploadMiddleware,
   validateDto(createOrganizationDto),
   organizationControllers.create
 );
--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 
 import { userControllers } from "./user.controllers.js";
-import { uploadFiles, verifyAuthRole } from "#middleware/index.js";
+import { uploadMiddleware, verifyAuthRole } from "#middleware/index.js";
 
 export const userRoutes = express.Router();
 
 userRoutes
   .get("/", userControllers.getAll)
   .get("/:id", userControllers.getById)
-  .patch("/:id", uploadFiles, userControllers.updateById)
+  .patch("/:id", uploadMiddleware, userControllers.updateById)
   .delete("/:id", verifyAuthRole("admin"), userControllers.deleteById);
