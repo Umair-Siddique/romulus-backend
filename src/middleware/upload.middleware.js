2ce697931f342f05b31efd5e06c74812b553eff3
Sharjeel Faiq <mrmalik610@gmail.com>
2025-06-17 15:25:07 +0500

    Add Twilio integration and refactor Cloudinary configuration into separate files

--- src/middleware/upload.middleware.js
+++ src/middleware/upload.middleware.js
@@ -1,35 +1,6 @@
 import multer from "multer";
-import path from "path";
-import { v2 as cloudinary } from "cloudinary";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
 
-import { env } from "#config/index.js";
-
-const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } =
-  env;
-
-cloudinary.config({
-  cloud_name: CLOUDINARY_CLOUD_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
-});
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: (req, file) => {
-    // Extract user ID from the form data
-    const userId = req.body.user;
-    const folderPath = `users/${userId}`;
-    const fileExtension = path.extname(file.originalname).substring(1);
-    const publicId = file.fieldname;
-
-    return {
-      folder: folderPath,
-      public_id: publicId,
-      format: fileExtension,
-    };
-  },
-});
+import { storage } from "#config/index.js";
 
 const knownFields = [
   { name: "profilePicture", maxCount: 1 },
