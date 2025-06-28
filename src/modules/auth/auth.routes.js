3c12b65ff2fa531e60eb904af742739ecbc487e3
Sharjeel Faiq <mrmalik610@gmail.com>
2025-06-28 22:11:00 +0500

    Add access token validation to signout route

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -14,7 +14,7 @@ export const authRoutes = express.Router();
 authRoutes
   .post("/signup", validate.dto(signUpDto), authControllers.signUp)
   .post("/signin", validate.dto(signInDto), authControllers.signIn)
-  .post("/signout", authControllers.signOut)
+  .post("/signout", validate.accessToken, authControllers.signOut)
   .post(
     "/forgot-password",
     validate.dto(forgotPasswordDto),
