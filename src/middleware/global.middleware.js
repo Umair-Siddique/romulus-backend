3e266d01a8d71be95b9781267c56431d91754562
Sharjeel Faiq <mrmalik610@gmail.com>
2025-07-03 16:23:30 +0500

    Increase JSON and URL-encoded data size limit in global middleware to 10mb

--- src/middleware/global.middleware.js
+++ src/middleware/global.middleware.js
@@ -85,8 +85,8 @@ export const applyGlobalMiddleware = (app, appRouter) => {
   app.use(morgan("dev"));
   app.use(cors(corsOptions));
   app.use(cookieParser());
-  app.use(express.json({ limit: "5mb" }));
-  app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+  app.use(express.json({ limit: "10mb" }));
+  app.use(express.urlencoded({ extended: true, limit: "10mb" }));
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   app.use(appRouter);
   app.use(invalidRouteHandler);
