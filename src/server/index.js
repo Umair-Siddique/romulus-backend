b88fd1fcc57e6e102630a4821111d4f654d369d7
Sharjeel Faiq <mrmalik610@gmail.com>
2025-07-17 22:33:49 +0500

    feat: Refactor server structure by moving app initialization to app.js and updating startServer logic in index.js; remove obsolete server.js file

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,41 @@
-import express from "express";
+import { createServer } from "node:http";
+import { Server as SocketIOServer } from "socket.io";
 
+import { connectDatabase } from "#config/index.js";
 import { applyGlobalMiddleware } from "#middleware/index.js";
+import { logger, env } from "#config/index.js";
+import { globalUtils } from "#utils/index.js";
 import appRouter from "#routes/index.js";
+import { app } from "./app.js";
 
-const app = express();
-applyGlobalMiddleware(app, appRouter);
+const { PORT } = env;
+const { asyncHandler } = globalUtils;
 
-export default app;
+const server = createServer(app);
+
+const startServer = asyncHandler(async () => {
+  await connectDatabase();
+
+  applyGlobalMiddleware(app, appRouter);
+
+  const io = new SocketIOServer(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  io.on("connection", (socket) => {
+    logger.info(`Client connected: ${socket.id}`);
+    socket.on("disconnect", () => {
+      logger.info(`Client disconnected: ${socket.id}`);
+    });
+  });
+
+  // You can now use `io.on("connection", ...)` here if needed
+
+  server.listen(PORT || 5000, () => {
+    logger.info(`Connection Established: http://localhost:${PORT}`);
+  });
+});
+
+export { startServer };
