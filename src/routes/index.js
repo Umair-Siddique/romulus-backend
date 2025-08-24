b71d119972233e1e02c9c67e1f2c3bfa2e6b01af
Sharjeel Faiq <mrmalik610@gmail.com>
2025-08-24 23:07:11 +0500

    added access token validation in chats routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,7 +21,7 @@ appRouter.use("/api/v1", v1Router);
 appRouter.use("/health", healthRoutes);
 
 v1Router.use("/auth", authRoutes);
-v1Router.use("/chats", chatRoutes);
+v1Router.use("/chats", validate.accessToken, chatRoutes);
 v1Router.use("/educators", validate.accessToken, educatorRoutes);
 v1Router.use("/email", emailRoutes);
 v1Router.use("/missions", validate.accessToken, missionRoutes);
